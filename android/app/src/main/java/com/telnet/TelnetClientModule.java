4a051d222f17a233045a244b9a411726b5c4d348
Ylli Petrovci <yllipetrovci@Yllis-MacBook-Pro.local>
2020-04-14 15:18:14 +0200

    fix disconnect telnetclinet when there isn't any connection, send as params telnet client params

--- android/app/src/main/java/com/telnet/TelnetClientModule.java
+++ android/app/src/main/java/com/telnet/TelnetClientModule.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.ReadableMap;
 
 // Internal depedencies
 import java.io.DataOutputStream;
@@ -26,99 +27,112 @@ import org.apache.commons.net.telnet.SuppressGAOptionHandler;
 import org.apache.commons.net.telnet.InvalidTelnetOptionException;
 
 public class TelnetClientModule extends ReactContextBaseJavaModule {
-  private static ReactApplicationContext reactContext;
-  public  TelnetClient telnetClient = null;
-  public  TelnetClientConfig telnetClientConfig = null;
-  static Thread reader = null;
-  static MessageHolder messageHolder = new MessageHolder("");
+    private static ReactApplicationContext reactContext;
+    public TelnetClient telnetClient = null;
+    public TelnetClientConfig telnetClientConfig = null;
+    static Thread reader = null;
+    static MessageHolder messageHolder = new MessageHolder("");
 
-  TelnetClientModule(ReactApplicationContext context) {
-    super(context);
-    reactContext = context;
-  }
+    TelnetClientModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+    }
 
     @Override
     public String getName() {
         return "TelnetClient";
     }
 
-    public void authenticate(TelnetClient telnetClient, TelnetClientConfig telnetConfig){
+    public void authenticate(TelnetClient telnetClient, TelnetClientConfig telnetConfig) {
         sendInternalCommand(telnetConfig.getUsername());
         sendInternalCommand(telnetConfig.getPassword());
     }
 
-    public  void sendInternalCommand(String command) {
+    public void sendInternalCommand(String command) {
         try {
-            //  ====
             OutputStream outputStream = this.telnetClient.getOutputStream();
             writeCommandToStream(outputStream, command);
             Thread.currentThread().sleep(1200); // TODO: Remove
-        } 
-        catch(InterruptedException e){
-          e.printStackTrace();
-        }
-        catch (IOException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @ReactMethod
-    public void connect(Callback onSuccessCallback, Callback onErrorCallback) {
-        telnetClientConfig = new TelnetClientConfig("209.73.216.51", 23,"gorjthatsmyrj","My3l@de.com");
+    public void connect(ReadableMap config,Callback onSuccessCallback, Callback onErrorCallback) {
+        //Parse the data fro
+        String ipAddress = config.getString("ipAddress");
+        String port = config.getString("port");
+        String username = config.getString("username");
+        String password = config.getString("password");
+
+        telnetClientConfig = new TelnetClientConfig(ipAddress, Integer.parseInt(port), username, password);
         telnetClient = new TelnetClient("VT100");
-        
+
         // Connect to telnet terminal
         try {
             this.telnetClient.connect(this.telnetClientConfig.getRemoteIpAddress(), this.telnetClientConfig.getPort());
             this.telnetClient.registerNotifHandler(new TelnetNotificationHandlerImp());
             this.addOptionalConnectionHandlers(this.telnetClient);
             Thread.currentThread().sleep(2000); // TODO: Remove
-            Thread reader = new Thread(new TelnetResponseMessageHandler(telnetClient,messageHolder,onSuccessCallback, onErrorCallback));
+            Thread reader = new Thread(
+                    new TelnetResponseMessageHandler(telnetClient, messageHolder, onSuccessCallback, onErrorCallback));
             reader.start();
             // Thread.currentThread().sleep(3000); // TODO: Remove
-        } 
-        catch(IOException e){
+        } catch (IOException e) {
 
             System.err.println("Exception while connecting:" + e.getMessage());
             onErrorCallback.invoke("Exception while connecting:" + e.getMessage());
-        
-        }
-        catch ( InterruptedException e) {
+
+        } catch (InterruptedException e) {
             System.err.println("Exception while connecting:" + e.getMessage());
             onErrorCallback.invoke("Exception while connecting:" + e.getMessage());
-        } 
-        catch( InvalidTelnetOptionException e){
-            onErrorCallback.invoke("invalid telnet exception"+ e.getMessage());
+        } catch (InvalidTelnetOptionException e) {
+            onErrorCallback.invoke("invalid telnet exception" + e.getMessage());
+        }catch (NumberFormatException e){
+            System.out.print("Number format exception");
         }
 
         if (!(this.telnetClientConfig.getUsername().isEmpty() || this.telnetClientConfig.getPassword().isEmpty())) {
             this.authenticate(this.telnetClient, this.telnetClientConfig);
         }
+        WritableNativeArray response = new WritableNativeArray();
+        response.pushString(ipAddress);
+        response.pushString(port);
+        response.pushString(username);
+        response.pushString(password);
 
-        onSuccessCallback.invoke("Connection successful");
+
+        // onSuccessCallback.invoke("Connection successful");
+        onSuccessCallback.invoke(response);
+
     }
 
     @ReactMethod
     private void disconnect(Callback onSuccessCallback, Callback onErrorCallback) {
         try {
-            telnetClient.disconnect();
-            onSuccessCallback.invoke("Successfully disconnected");
+            if(telnetClient != null){
+                telnetClient.disconnect();
+            }
+                onSuccessCallback.invoke("Successfully disconnected");
         } catch (IOException e) {
             System.err.println("Exception while connecting:" + e.getMessage());
             onErrorCallback.invoke(String.format("Failed to disconnected: {}", e.getMessage()));
         } finally {
-            reader.interrupt();
+            // if(render != null) reader.interrupt();
+            // we should look when we have to interrupt reader thread
         }
     }
 
-   @ReactMethod
-   public  void sendCommand(String command, Callback onSuccessCallback, Callback onErrorCallback) {
-        Thread executingCommandThread = new Thread(new Runnable(){
+    @ReactMethod
+    public void sendCommand(String command, Callback onSuccessCallback, Callback onErrorCallback) {
+        Thread executingCommandThread = new Thread(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 try {
-                    
+
                     messageHolder.reset();
                     OutputStream outputStream = telnetClient.getOutputStream();
                     writeCommandToStream(outputStream, command);
@@ -130,44 +144,46 @@ public class TelnetClientModule extends ReactContextBaseJavaModule {
                     while (stringTokenizer.hasMoreTokens()) {
                         String line = stringTokenizer.nextToken("\r\n");
                         System.out.println(line);
-                        if (line.equals(command)) continue;
+                        if (line.equals(command))
+                            continue;
                         if (line.contains("Unknown command or computer name")) {
                             onErrorCallback.invoke(line);
                             break;
                         }
 
-                        if (!line.isEmpty()) response.pushString(line);
-                    };
+                        if (!line.isEmpty())
+                            response.pushString(line);
+                    }
+                    ;
 
                     onSuccessCallback.invoke(response);
-                } 
-                catch(InterruptedException e){
-                e.printStackTrace();
-                onErrorCallback.invoke("Failed to send command, something went wrong. Please try again!");
-                }
-                catch (IOException e) {
-                onErrorCallback.invoke("Failed to send command, something went wrong. Please try again!");
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }        
+                    onErrorCallback.invoke("Failed to send command, something went wrong. Please try again!");
+                } catch (IOException e) {
+                    onErrorCallback.invoke("Failed to send command, something went wrong. Please try again!");
+                    e.printStackTrace();
+                }
 
             }
         });
-        try{
-        executingCommandThread.join();
-        executingCommandThread.start();
-        }catch(InterruptedException e){
+        try {
+            executingCommandThread.join();
+            executingCommandThread.start();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     private void writeCommandToStream(OutputStream outstr, String command) throws IOException {
-        byte[] weather2Bytes = (command + "\r\n").getBytes();
-        int weather2BytesCounter = command.length() + 1;
-        outstr.write(weather2Bytes, 0 , weather2BytesCounter);
+        byte[] commandBytes = (command + "\r\n").getBytes();
+        int commandBytesLength = command.length() + 1;
+        outstr.write(commandBytes, 0, commandBytesLength);
         outstr.flush();
     }
-    
-    public  void addOptionalConnectionHandlers(TelnetClient telnetClient) throws InvalidTelnetOptionException, IOException {
+
+    public void addOptionalConnectionHandlers(TelnetClient telnetClient)
+            throws InvalidTelnetOptionException, IOException {
         TerminalTypeOptionHandler ttopt = new TerminalTypeOptionHandler("VT100", false, false, true, false);
         EchoOptionHandler echoopt = new EchoOptionHandler(true, false, true, false);
         SuppressGAOptionHandler gaopt = new SuppressGAOptionHandler(true, true, true, true);
